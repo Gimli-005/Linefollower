b67aad3caada36d25392e4922e44b8e1a0006428
Robbe <114517587+Gimli-005@users.noreply.github.com>
2022-11-29 19:55:10 +0100

    Add files via upload

--- code/proof of concepts/hbridge/Aansturing_H-brug.ino
+++ code/proof of concepts/hbridge/Aansturing_H-brug.ino
@@ -1,77 +1,82 @@
-int BENABLE = 8; 
-int BPHASE =  9;
-int AENABLE = 7; 
-int APHASE = 6;
+int BENABLE = 9; 
+int BPHASE =  8;
+int AENABLE = 6; 
+int APHASE = 7;
+int MODE = 4;
 //int POTB = A0;
 //int POTA= A1;
 //int valA = 0;
 //int valB = 0;         
 //int Richting = 1;
-
 void setup()
 {
   pinMode(BENABLE, OUTPUT);
   pinMode (BPHASE, OUTPUT);
 
   pinMode(AENABLE, OUTPUT);
-  pinMode (APHASE, OUTPUT);    
+  pinMode (APHASE, OUTPUT);
+    
 }
 
 void loop()
 {
+  digitalWrite(MODE, HIGH);
+  
   //Motor 1 vooruit, traag-snel-traag
-  analogWrite(BENABLE,0);
-  for(int i = 0; i<256; i++)
+  digitalWrite(BPHASE,0);
+  for(int i = 0; i<255; i++)
   {
-    analogWrite(BPHASE,i);
+    analogWrite(BENABLE,i);
     delay(5);
   }
 
-  for(int i = 256; i>0; i--)
+  for(int i = 255; i>0; i--)
   {
-    analogWrite(BPHASE,i);
+    analogWrite(BENABLE,i);
     delay(5);
   }
 
 //Motor 1 achteruit, traag-snel-traag
-  analogWrite(BENABLE,1);
-  for(int i = 0; i<256; i++)
+  digitalWrite(BPHASE,1);
+  for(int i = 0; i<255; i++)
   {
-    analogWrite(BPHASE,i);
+    analogWrite(BENABLE,i);
     delay(5);
   }
 
-  for(int i = 256; i>0; i--)
+  for(int i = 255; i>0; i--)
   {
-    analogWrite(BPHASE,i);
+    analogWrite(BENABLE,i);
     delay(5);
   }
 
-
+ delay(500); 
+ 
 //Motor 2 vooruit, traag-snel-traag
-  analogWrite(AENABLE,0);
-  for(int i = 0; i<256; i++)
+  digitalWrite(APHASE,0);
+  for(int i = 0; i<255; i++)
   {
-    analogWrite(APHASE,i);
+    analogWrite(AENABLE,i);
     delay(5);
   }
 
-  for(int i = 256; i>0; i--)
+  for(int i = 255; i>0; i--)
   {
-    analogWrite(APHASE,i);
+    analogWrite(AENABLE,i);
     delay(5);
   }
 
 //Motor 2 achteruit, traag-snel-traag
-  analogWrite(AENABLE,1);
-  for(int i = 0; i<256; i++)
+  digitalWrite(APHASE,1);
+  for(int i = 0; i<255; i++)
   {
-    analogWrite(APHASE,i);
+    analogWrite(AENABLE,i);
     delay(5);
   }
-   for(int i = 256; i>0; i--)
+   for(int i = 255; i>0; i--)
   {
-    analogWrite(APHASE,i);
+    analogWrite(AENABLE,i);
     delay(5);
-  }  
+  } 
+  delay(500);    
 }
